f8c578467b2e4d9bb090f559cc05cf523dce5486
fitrah17 <41928410+fitrah17@users.noreply.github.com>
2023-02-17 10:42:15 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000001",
+    "tg":  "0123456789abcdef0123456789abcdef",
     # "tg2": "0123456789abcdef0123456789abcdef",
 }
 
